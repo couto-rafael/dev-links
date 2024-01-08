555d92e542e4a01460f05e50a5e9782bc639cbf4
Rafael Couto <couto.rafael@gmail.com>
2024-01-08 20:27:52 -0300

    switch para light mode

---
+++ script.js
@@ -0,0 +1,24 @@
+function toggleMode() {
+  const html = document.documentElement;
+
+  html.classList.toggle("light");
+  // if (html.classList.contains("light")) {
+  //   html.classList.remove("light");
+  // } else {
+  //   html.classList.add("light");
+  // }
+
+  // TUDO ISSO ACIMA É O MESMO O TOGGLE
+
+  const img = document.querySelector("#profile img");
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar.png");
+  } else {
+    img.setAttribute("src", "./assets/avatar.png");
+  }
+
+  // pegar a tam img e substituir a imagem
+  // se tiver light mode, adicionar a imagem light
+  // se tiver sem light mode, deixar imagem normal
+}
