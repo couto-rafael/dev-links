96fb46ee7eff033c16317fa09461e8d25bd4cd6e
Rafael Couto <couto.rafael@gmail.com>
2024-01-09 13:30:22 -0300

    alterada foto

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function toggleMode() {
   const img = document.querySelector("#profile img");
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png");
+    img.setAttribute("src", "./assets/avatar.png");
   } else {
     img.setAttribute("src", "./assets/avatar.png");
   }
